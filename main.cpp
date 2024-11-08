6ae1ea680df9809c10e20f0e7296af7bc2ac1dce
Jan Czaja | EleJanDC2 <elejandc.2@gmail.com>
2024-11-08 17:52:16 +0100

    feat: added argument loading

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-int main()
+// Local_network_db_ip Local_network_db_port Global_network_db_ip Global_network_db_port db_password frontend_websocket_port robots_websocket_port
+
+#define REQUESTED_ARGUMENTS 8
+
+int main(int argc, const char * argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "INFO:RobotControlCenterBackend - START" << std::endl;
+
+    if(argc != REQUESTED_ARGUMENTS) {
+        std::cerr << "ERROR : Wrong number of arguments" << std::endl;
+        std::cerr << "ERROR : expected: RobotControlCenterBackend Local_network_db_ip Local_network_db_port Global_network_db_ip Global_network_db_port db_password frontend_websocket_port robots_websocket_port" << std::endl;
+        return 1;
+    } else {
+        std::cout << "INFO : Arguments OK" << std::endl;
+    }
+
+    std::cout << "INFO:RobotControlCenterBackend - END" << std::endl;
     return 0;
 }
