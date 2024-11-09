874106e2cf50d9f6b335152933ebda0bfe4add67
Jan Czaja | EleJanDC2 <elejandc.2@gmail.com>
2024-11-09 09:02:11 +0100

    feat: create singleton RobotManager class to manage robots informations and statuses

---
+++ Robots/RobotManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by elejandc on 11/9/24.
+//
+
+#include "RobotManager.h"
+
+RobotManager::RobotManager() {
+
+}
+
+RobotManager::~RobotManager() {
+
+}
---
+++ Robots/RobotManager.h
@@ -0,0 +1,27 @@
+//
+// Created by elejandc on 11/9/24.
+//
+
+#ifndef ROBOTMANAGER_H
+#define ROBOTMANAGER_H
+
+
+
+class RobotManager {
+public:
+    RobotManager(const RobotManager&) = delete;
+    RobotManager& operator=(const RobotManager&) = delete;
+
+    static RobotManager& getInstance() {
+        static RobotManager _instance;
+        return _instance;
+    }
+private:
+    RobotManager();
+    ~RobotManager();
+
+};
+
+
+
+#endif //ROBOTMANAGER_H
