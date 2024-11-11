331f332e0341ece4b326f2c3f5630acf0f2eae0a
Jan Czaja | EleJanDC2 <elejandc.2@gmail.com>
2024-11-11 12:29:25 +0100

    feat: Init App

--- App.cpp
+++ App.cpp
@@ -8,6 +8,8 @@
 
 #include "Robots/RobotManager.h"
 
+std::unique_ptr<App> App::_instance = nullptr;
+
 App::App(int argc, const char *argv[])
     : _db_local_ip(argv[1]), _db_local_port(argv[2]), _db_remove_ip(argv[3]), _db_remove_port(argv[4]), _db_password(argv[5]), _frontend_websocket_port(argv[6]), _robots_websocket_port(argv[7]){
     std::cout << "DEBUG : App - CONSTRUCTOR" << std::endl;
