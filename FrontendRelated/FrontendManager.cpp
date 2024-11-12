38598dbc12b6d1a12b8c51a8967b4dbdeeb2e89c
Jan Czaja | EleJanDC2 <elejandc.2@gmail.com>
2024-11-12 09:31:15 +0100

    feat: Create singleton FrontendManager class to handle frontend actions

--- FrontendRelated/FrontendManager.cpp
+++ FrontendRelated/FrontendManager.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "FrontendManager.h"
+
+
+FrontendManager::FrontendManager(asio::io_context &ioc, unsigned short _robot_connection_port)
+    : _ioc(ioc) {
+    std::cout << "DEBUG : FrontendManager - CONSTRUCTOR" << std::endl;
+}
+
+FrontendManager::~FrontendManager() {
+    std::cout << "DEBUG : FrontendManager - DESTRUCTOR" << std::endl;
+}
--- FrontendRelated/FrontendManager.h
+++ FrontendRelated/FrontendManager.h
@@ -5,10 +5,41 @@
 #ifndef FRONTENDMANAGER_H
 #define FRONTENDMANAGER_H
 
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/websocket.hpp>
+#include <iostream>
+#include <boost/asio/steady_timer.hpp>
 
+namespace asio = boost::asio;
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+using tcp = asio::ip::tcp;
 
 class FrontendManager {
+public:
+    FrontendManager(const FrontendManager&) = delete;
+    FrontendManager& operator=(const FrontendManager&) = delete;
+    ~FrontendManager();
 
+    static bool initInstance(asio::io_context &ioc, unsigned short _robot_connection_port) {
+        if(!_instance) {
+            _instance = std::unique_ptr<FrontendManager>(new FrontendManager(ioc, _robot_connection_port));
+        }
+        return _instance != nullptr;
+    }
+
+    static FrontendManager& getInstance() {
+        return *_instance;
+    }
+
+private:
+    static std::unique_ptr<FrontendManager> _instance;
+    asio::io_context &_ioc;
+
+    FrontendManager(asio::io_context &ioc, unsigned short _robot_connection_port);
+
+    void onNewFrontendConnection(websocket::stream<tcp::socket> robot_websocket);
 };
 
 
