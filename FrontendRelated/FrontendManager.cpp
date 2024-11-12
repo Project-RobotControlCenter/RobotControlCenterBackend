802925904734b921d4874e4f006e956cab277c1c
Jan Czaja | EleJanDC2 <elejandc.2@gmail.com>
2024-11-12 09:23:59 +0100

    feat: Create singleton FrontendConnectionListener class to handle Frontend Connections

---
+++ FrontendRelated/FrontendManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elejandc on 11/12/24.
+//
+
+#include "FrontendManager.h"
---
+++ FrontendRelated/FrontendManager.h
@@ -0,0 +1,16 @@
+//
+// Created by elejandc on 11/12/24.
+//
+
+#ifndef FRONTENDMANAGER_H
+#define FRONTENDMANAGER_H
+
+
+
+class FrontendManager {
+
+};
+
+
+
+#endif //FRONTENDMANAGER_H
